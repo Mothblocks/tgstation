12e858394795dbb181376d33391df5453085f7c6
MonkeyThatCodes <monkey>
2021-09-11 00:15:09 +0200

    shit

--- tgui/packages/tgui/interfaces/PreferencesMenu/JobsPage.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/JobsPage.tsx
@@ -371,8 +371,8 @@ export const JobsPage = () => {
 
         <Stack.Item>
           <Stack fill className="PreferencesMenu__Jobs">
-            <Stack.Item>
-              <Gap amount={66} />
+            <Stack.Item mr={1}>
+              <Gap amount={36} />
 
               <PriorityHeaders />
 
@@ -399,7 +399,7 @@ export const JobsPage = () => {
                 name="Assistant" />
             </Stack.Item>
 
-            <Stack.Item>
+            <Stack.Item mr={1}>
               <PriorityHeaders />
               <Department department={Departments.Captain} name="Captain" />
               <Gap amount={6} />
@@ -409,7 +409,7 @@ export const JobsPage = () => {
             </Stack.Item>
 
             <Stack.Item>
-              <Gap amount={66} />
+              <Gap amount={36} />
 
               <PriorityHeaders />
 
--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/species/ethereal.ts
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/species/ethereal.ts
@@ -1,15 +1,18 @@
 import { createLanguagePerk, Species } from "./base";
+import { multiline } from 'common/string';
 
 const Ethereal: Species = {
-  description: "Coming from the planet of Sprout, the theocratic ethereals are \
-    separated socially by caste, and espouse a dogma of aiding the weak and \
-    downtrodden.",
+  description: multiline`Coming from the planet of Sprout, the theocratic
+  ethereals are separated socially by caste. They are organic creatures
+  with bio-luminescent skin and electrically charged blood, and are known
+  for their partial-immortality and bright nature.`,
   features: {
     good: [{
       icon: "bolt",
       name: "Shockingly Tasty",
-      description: "Ethereals can feed on electricity from APCs, and do not \
-        otherwise need to eat.",
+      description: multiline`Ethereals can feed on electricity from APCs,
+      borg chargers, lights. They can also eat food with enriched liquid
+      electricity inside of it. Normal food does nothing for them though.`,
     }, {
       icon: "lightbulb",
       name: "Disco Ball",
@@ -21,8 +24,8 @@ const Ethereal: Species = {
     }, {
       icon: "temperature-high",
       name: "Heat Resistance",
-      description: "Ethereals have much better tolerance for high \
-        temperatures.",
+      description: multiline`Ethereals have much better tolerance for high \
+        temperatures.`,
     }, createLanguagePerk("Voltaic")],
     neutral: [{
       icon: "tint",
@@ -52,11 +55,17 @@ const Ethereal: Species = {
     }, {
       icon: "temperature-low",
       name: "Cold Weakness",
-      description: "Ethereals have much lower tolerance for cold \
-        temperatures.",
+      description: "Ethereals have much lower tolerance for cold temperatures.",
     }],
   },
-  lore: "LORE MASTER HELP",
+  lore: multiline`Ethereals are a species native to the planet Sprout. When
+  they were originally discovered, they were at a medieval level of
+  technological progression, but due to their natural acclimation with
+  electricity, they felt easy among the large NanoTrasen installations.
+
+  Due to a hostile take-over by the millitary caste on their home planet,
+  most Ethereals are refugees that fled from their home, the ones that are
+  still on Sprout are working there under authoritian rule.`,
 };
 
 export default Ethereal;
